3be35f14405fbca9f35e9ed396535813fe30e772
Aakash251224 <mdakashtalukder3@gmail.com>
2026-05-21 13:55:53 +0500

    Implement Food class with random spawning logic

---
+++ Food.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Food implements GameObject {
+
+    private Point position;
+    private Random random = new Random();
+
+    public Food() {
+        position = new Point(random.nextInt(20), random.nextInt(20));
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void respawn() {
+        position = new Point(random.nextInt(20), random.nextInt(20));
+    }
+
+    @Override
+    public void update() {}
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(position.x * 20, position.y * 20, 20, 20);
+    }
+}
